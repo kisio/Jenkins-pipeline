5267f598d799eac5951400b14881e1b399d4503d
kisio <derrickkisio@gmail.com>
2025-04-03 05:47:47 +0300

    Configured MongoDB Atlas

--- _config.js
+++ _config.js
@@ -5,5 +5,4 @@ config.mongoURI = {
     development: 'mongodb+srv://derrick:derrickdevopstest@cluster0.bm24bxo.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://derrick:derrickdevopstest@cluster0.bm24bxo.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
-
 module.exports = config;
